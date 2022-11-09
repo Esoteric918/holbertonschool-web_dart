e8a1b0b0bf03301ca5d3592ae568393ccf39962b
esoteric918 <3411@holbertonstudent.com>
2022-11-09 12:42:12 -0600

    task 4

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,19 @@
+// The output of the program should be:
+
+//     3 times the value of str
+//     followed by the 9 first characters of str
+//     followed by a new line
+
+// You are not allowed to use any loops or conditional statement
+// Your program should be maximum 5 lines long
 
 
 void main() {
   String str = "Holberton School";
+  print(str * 3 + str.substring(0, 9) + "\n");
 }
+
+
+// void main() {
+//   String str = "Holberton School";
+// }
