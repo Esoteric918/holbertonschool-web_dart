858e46a8f182c75d4fd752d866cee563423e578d
esoteric918 <3411@holbertonstudent.com>
2022-11-09 13:12:54 -0600

    task 5

--- 5-assertion.dart
+++
@@ -1,5 +0,0 @@
-void main(List<String> args) {
-    // make it check if the number is bigger or equal to 80 print You Passed otherwise the output should be Uncaught Error: Assertion failed: "The score must be bigger or equal to 80
-    assert(args[0] >= 80, "The score must be bigger or equal to 80");
-    print("You Passed");
-}
