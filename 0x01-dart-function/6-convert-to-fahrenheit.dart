dd91ab7e89051be83e5c6d3ee2bfe71324b68ce2
esoteric918 <3411@holbertonstudent.com>
2022-11-10 17:01:57 -0600

    task 6

---
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -0,0 +1,10 @@
+
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC.map((temp) => double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2))).toList();
+}
+
+main() {
+  print(convertToF([25, 26, 23, 27, 30]));
+  print(convertToF([22.5, 26, 27.3, 23.6, 25]));
+  print(convertToF([-4.2, -2, 0.5, -5, -1.7]));
+}
