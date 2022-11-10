263d01aa8a67729d2bf3aceeca58fbc744c43cd4
esoteric918 <3411@holbertonstudent.com>
2022-11-10 09:46:20 -0600

    task 2

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -5,16 +5,7 @@ int add(a, b) {
 int sub(a, b) {
   return a - b;
 }
-// str => String : Contain the output With new line between Add and Sub
-// Return str
 String showFunc(int a, int b) {
   String str = "Add ${a} + ${b} = ${add(a, b)}\nSub ${a} + ${b} = ${sub(a, b)}";
   return str;
-}
-
-
-void main(List<String> args) {
-  var a = int.parse(args[0]);
-  var b = int.parse(args[1]);
-  print(showFunc(a, b));
 }
