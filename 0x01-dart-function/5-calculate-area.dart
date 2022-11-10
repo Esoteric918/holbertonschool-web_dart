c461e8169c2978bcfffa2dfb9befdd76e421ac50
esoteric918 <3411@holbertonstudent.com>
2022-11-10 15:22:18 -0600

    task 5?

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,11 @@
+double calculateArea(double height, double base) {
+  // the return shoud be rounded to two decimal places
+  return double.parse((0.5 * height * base).toStringAsFixed(2));
+}
+
+
+main() {
+  print(calculateArea(20,7));
+  print(calculateArea(10,7));
+  print(calculateArea(8.5,3.7));
+}
