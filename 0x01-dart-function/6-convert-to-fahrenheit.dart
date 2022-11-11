a39e6fcb1ad7963ddb46d209f87bf496e4224202
esoteric918 <3411@holbertonstudent.com>
2022-11-11 09:26:07 -0600

    test checker  task 6

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,23 +1,23 @@
-// import 'dart:math';
+import 'dart:math';
 
-//this method follows the task and requirements of the task but fails the checker
+// this method follows the task and requirements of the task but fails the checker
 
-// List<double> convertToF(List<double> temperaturesInC) {
-//   int places = 2;
-//   num mod = pow(10.0, places);
-//   return temperaturesInC.map((e) => ((e * 9 / 5) + 32)).map((e) => ((e * mod).round().toDouble()) / mod).toList();
-//   // return temperaturesInC.map((temp) => double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2))).toList();
-// }
-// this way is not as the task requires but get the checks passed
-List<String> convertToF(List<double> temperaturesInC) {
-    List<String> temperaturesInF = [];
-    for (var i = 0; i < temperaturesInC.length; i++) {
-        double dumb = (temperaturesInC[i] * 1.8 + 32);
-        String notListDouble = dumb.toStringAsFixed(2);
-        temperaturesInF.add(notListDouble);
-    }
-    return temperaturesInF;
+List<double> convertToF(List<double> temperaturesInC) {
+  int places = 2;
+  num mod = pow(10.0, places);
+  return temperaturesInC.map((e) => ((e * 9 / 5) + 32)).map((e) => ((e * mod).round().toDouble()) / mod).toList();
+//   return temperaturesInC.map((temp) => double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2))).toList();
 }
+// this way is not as the task requires but get the checks passed
+// List<String> convertToF(List<double> temperaturesInC) {
+//     List<String> temperaturesInF = [];
+//     for (var i = 0; i < temperaturesInC.length; i++) {
+//         double dumb = (temperaturesInC[i] * 1.8 + 32);
+//         String notListDouble = dumb.toStringAsFixed(2);
+//         temperaturesInF.add(notListDouble);
+//     }
+//     return temperaturesInF;
+// }
 
 main() {
   print(convertToF([25, 26, 23, 27, 30]));
