f0171982fddec138f4037ba486e496ed1dcc0583
esoteric918 <3411@holbertonstudent.com>
2022-11-12 16:36:37 -0600

    task 8

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -1,22 +1,42 @@
 String longestUniqueSubstring(String s) {
   var longest = '';
   var current = '';
-// use the compareTo() method to compare the characters
   for (var i = 0; i < s.length; i++) {
-    if (current.compareTo(s[i]) < 0) {
-      current += s[i];
+    var c = s[i];
+    var index = current.indexOf(c);
+    if (index == -1) {
+      current += c;
     } else {
       if (current.length > longest.length) {
         longest = current;
       }
-      current = s[i];
+      current = current.substring(index + 1) + c;
     }
   }
-  return longest.length > current.length ? longest : current;
+  if (current.length > longest.length) {
+    longest = current;
+  }
+  return longest;
 }
+//   var longest = '';
+//   var current = '';
+// // use the compareTo() method to compare the characters
+//   for (var i = 0; i < s.length; i++) {
+//     if (current.compareTo(s[i]) < 0) {
+//       current += s[i];
+//     } else {
+//       if (current.length > longest.length) {
+//         longest = current;
+//       }
+//       current = s[i];
+//     }
+//   }
+//   return longest.length > current.length ? longest : current;
+
 
 main() {
   print(longestUniqueSubstring("abcacbd"));
   print(longestUniqueSubstring("aaaaaaaa"));
   print(longestUniqueSubstring("abcde"));
+  print(longestUniqueSubstring('banBan'));
 }
