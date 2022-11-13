befb78657bab32b494a705e455d6274b3cf4013a
esoteric918 <3411@holbertonstudent.com>
2022-11-12 23:32:01 -0600

    task 8

--- 0x01-dart-function/8-longest-unique-substring.dart
+++ 0x01-dart-function/8-longest-unique-substring.dart
@@ -3,14 +3,13 @@ String longestUniqueSubstring(String s) {
   var current = '';
   for (var i = 0; i < s.length; i++) {
     var c = s[i];
-    var index = current.indexOf(c);
-    if (index == -1) {
+    if (current.compareTo(c) < 0) {
       current += c;
     } else {
       if (current.length > longest.length) {
         longest = current;
       }
-      current = current.substring(index + 1) + c;
+      current = c;
     }
   }
   if (current.length > longest.length) {
@@ -18,21 +17,6 @@ String longestUniqueSubstring(String s) {
   }
   return longest;
 }
-//   var longest = '';
-//   var current = '';
-// // use the compareTo() method to compare the characters
-//   for (var i = 0; i < s.length; i++) {
-//     if (current.compareTo(s[i]) < 0) {
-//       current += s[i];
-//     } else {
-//       if (current.length > longest.length) {
-//         longest = current;
-//       }
-//       current = s[i];
-//     }
-//   }
-//   return longest.length > current.length ? longest : current;
-
 
 main() {
   print(longestUniqueSubstring("abcacbd"));
