95319894b90ac293e5a2378feee9ee3ab19c1eb9
esoteric918 <3411@holbertonstudent.com>
2022-11-13 01:14:06 -0600

    task 10

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,26 @@
+import '9-palindrome.dart';
+
+String longestPalindrome(String s) {
+  if (s.length == 2) {
+   return 'none';
+  }
+  // if (isPalindrome(s)) {
+  //   return s;
+  // }
+  String longest = '';
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i + 1; j < s.length; j++) {
+      String sub = s.substring(i, j + 1);
+      if (isPalindrome(sub) && sub.length > longest.length) {
+        longest = sub;
+      }
+    }
+  }
+  return longest;
+}
+
+main() {
+  print(longestPalindrome("abcde"));
+  print(longestPalindrome("azeghjhg"));
+  print(longestPalindrome("aaabvf"));
+}
