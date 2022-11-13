7c384f66f35d00478df51909f54e2f9590ff2d22
esoteric918 <3411@holbertonstudent.com>
2022-11-13 00:52:14 -0600

    tsk 9

--- 0x01-dart-function/9-palindrome.dart
+++ 0x01-dart-function/9-palindrome.dart
@@ -1,17 +1,28 @@
+// bool isPalindrome(String s) {
+//   if (s.length == 2) {
+//     return false;
+//   }
+//   var i = 0;
+//   var j = s.length - 1;
+//   while (i < j) {
+//     if (s[i] != s[j]) {
+//       return false;
+//     }
+//     i++;
+//     j--;
+//   }
+//   return true;
+// }
+
 bool isPalindrome(String s) {
   if (s.length == 2) {
     return false;
   }
-  var i = 0;
-  var j = s.length - 1;
-  while (i < j) {
-    if (s[i] != s[j]) {
-      return false;
-    }
-    i++;
-    j--;
+  String? reversed = s.split('').reversed.join();
+  if (s == reversed) {
+    return true;
   }
-  return true;
+  return false;
 }
 
 main() {
