969247552793c6fd6af8e0826eeac938d60f45da
esoteric918 <3411@holbertonstudent.com>
2022-11-14 15:56:50 -0600

    task 2

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,14 +1,4 @@
-import '1-get_user_id.dart';
-
-
-Future<void> getUser() async {
-  try {
-    final String userData = await fetchUserData();
-    print(userData);
-  } catch (e) {
-    print('error caught: $e');
-  }
-}
+// import '1-get_user_id.dart';
 
 Future<String> fetchUser() =>
     // Imagine that this function is
@@ -18,6 +8,14 @@ Future<String> fetchUser() =>
       () => throw 'Cannot locate user',
     );
 
+Future<void> getUser() async {
+  try {
+    final String userData = await fetchUserData();
+    print(userData);
+  } catch (e) {
+    print('error caught: $e');
+  }
+}
 main() async {
   getUser();
 }
