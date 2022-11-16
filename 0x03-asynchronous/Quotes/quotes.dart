d7143f9e0fe19c0536c0fd4be9673126b11fc065
esoteric918 <3411@holbertonstudent.com>
2022-11-15 21:30:35 -0600

    task 6

--- 0x03-asynchronous/Quotes/quotes.dart
+++ 0x03-asynchronous/Quotes/quotes.dart
@@ -1,21 +1,11 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<String> generateQuote(id) async {
-  try {
-    final response = await http
-        .get(Uri.parse('https://breakingbadapi.com/api/quote/random'));
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      return data[0]['author'] + ' : ' + data[0]['quote'];
-    } else {
-      return 'Error: ${response.statusCode}';
-    }
-  } catch (e) {
-    return "There are no quotes";
-  }
-}
+Future<String> generateQuote(id) => http
+    .get(Uri.parse('https://breakingbadapi.com/api/quotes'))
+    .then((res) =>
+        "${jsonDecode(res.body)[--id]['author']} : ${jsonDecode(res.body)[id]['quote']}")
+    .catchError((err) => 'There are no quotes');
 
 main() async {
   print(await generateQuote(1));
